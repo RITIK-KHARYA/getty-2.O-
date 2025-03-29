d01ce887405865233e32dfdfee3cfbd9743a1bea
ritik <ritikkharya05@gmail.com>
2025-03-30 00:49:34 +0530

    feat:[conversation server action updated]

--- src/actions/conversation.ts
+++ src/actions/conversation.ts
@@ -1,17 +1,65 @@
-import { headers } from "next/headers";
+"use server";
 
-export default async function GetConversation(conservationId: string) {
-  //   const { isMember, userId, spaceId, isGroup, name} = data;
+import { headers } from "next/headers";
+import { getSession } from "./session";
+
+export default async function GetConversation() {
+
+  const response = await fetch("http://localhost:3000/api/conversation", {
+    headers: {
+      cookie: (await headers()).get("cookie") || "",
+    },
+    method: "GET",
+  });
+  const data = await response.json();
+
+  if (!response.ok) {
+    console.log("nothing to fetch", data);
+    return null;
+  }
+
+  console.log(data,"conversation here")
+  return data;
+}
+
+export async function CreateConversation(data: any) {
+  const currentUser = await getSession();
+  const onBoardinguser = data             //consider the userid2
+
+  const response = await fetch("http://localhost:3000/api/conversation", {
+    headers: {
+      "Content-Type": "application/json",
+      cookie: (await headers()).get("cookie") || "",
+    },
+    method: "POST",
+    body: JSON.stringify({ onBoardinguser, currentUser }),
+  });
+
+  if (!response.ok) {
+    console.log("error unable to create the conversation");
+    return null;
+  }
+
+  const value = await response.json();
+  console.log("conversation created,", value);
+  return value.data;
+}
+
+
+export async function DeleteConversation(ConversationId: string) {
   const response = await fetch(
-    `http://localhost:3000/api/conversation/${conservationId}`,
+    `http://localhost:3000/api/conversation/${ConversationId}`,
     {
       headers: {
         cookie: (await headers()).get("cookie") || "",
       },
-      method: "POST",
-      // body: JSON.stringify(data),
+      method: "DELETE",
     }
   );
   const data = await response.json();
-  return data.data;
+
+  if (!response) {
+    console.log("there is nothing to delete or either something went wrong");
+  }
+
 }
